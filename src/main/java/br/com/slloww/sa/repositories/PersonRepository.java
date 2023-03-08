3a2257c8d1fa964ecda935e6123f510e38e1d53f
vinis1910 <viniciusgabrielmartins79@gmail.com>
2023-03-08 10:35:55 -0300

    Atualizando

--- src/main/java/br/com/slloww/sa/repositories/PersonRepository.java
+++ src/main/java/br/com/slloww/sa/repositories/PersonRepository.java
@@ -1,8 +1,12 @@
 package br.com.slloww.sa.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.slloww.sa.entities.Person;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
+	Optional<Person> findByEmail(String email);
+	Optional<Person> findByPhone(String telefone);
 }
